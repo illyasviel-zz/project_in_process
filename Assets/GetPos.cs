086ad0eddc36b0421f534f99627376e9eb25819f
illyasviel <sma049@uottawa.ca>
2015-11-11 16:09:30 -0500

    import

---
+++ Assets/GetPos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetPos : MonoBehaviour {
+
+    Vector2 touchpos;
+    public GameObject player;
+    public Vector3 desPos;
+    Vector3 generatePos;
+	
+	void Start()
+    {
+        generatePos = new Vector3(-17f, 11.9f, 25f);
+    }
+    // Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchpos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(touchpos);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.name);
+                Debug.Log(hit.point);
+                if(hit.collider.name == "Floor")
+                {
+                    Instantiate(player, generatePos, Quaternion.identity);
+                    desPos = hit.point;
+                }
+            }
+
+        }
+
+	}
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+    GetPos getPos;
+    NavMeshAgent nav;
+    Animator ani;
+    Vector3 startDes;
+
+    bool newPlayer = true;
+
+    //PlayerPrefs 
+	
+    // Use this for initialization
+	void Start () {
+        getPos = Camera.main.GetComponent<GetPos>();
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        ani.SetBool("isWalking", true);
+        ani.speed = 5f;
+        startDes = getPos.desPos;
+        nav.SetDestination(startDes);
+        nav.stoppingDistance = 0.0f;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
